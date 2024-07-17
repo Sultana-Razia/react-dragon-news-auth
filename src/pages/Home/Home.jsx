5d324d1b6464074b7721355709418f26e26d5d45
Sultana Razia <sultanarazia483@gmail.com>
2024-07-17 18:33:36 +0600

    Custom Tailwind Class And Font Setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h2>This is Home</h2>
         </div>
     );
---
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2>Right SideNav</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
