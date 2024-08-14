31d72cc9bbaedc176f8e4b06ea82ec9bc11fa8b6
Sultana Razia <sultanarazia483@gmail.com>
2024-08-14 08:59:28 +0600

    Homepage layout and date time formatting using moment.js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
@@ -7,7 +9,19 @@ const Home = () => {
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h2>This is Home</h2>
+            <h2 className="text-3xl font-bold">This is Home</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h2 className="text-4xl">News coming soon......</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,11 @@
-
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="text-center">
+            <h1 className="unifrakturcook-bold mt-5 text-6xl text-[#444444]">THe Dragon News</h1>
+            <h2 className="text-[#706F6F] text-lg mt-5">Journalism Without Fear or Favour</h2>
+            <p className="text-[#706F6F] font-medium text-xl mt-3">{moment().format("dddd, MMMM DD YYYY, h:mm:ss a")}</p>
         </div>
     );
 };
