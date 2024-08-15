8d629cce72ed811ed81a00dd4c0b9881f9869fd3
Sultana Razia <sultanarazia483@gmail.com>
2024-08-15 12:08:08 +0600

    Right navigation and info with icons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
                 <div className="md:col-span-2 border">
                     <h2 className="text-4xl">News coming soon......</h2>
                 </div>
-                <div className="border">
+                <div className="">
                     <RightSideNav></RightSideNav>
                 </div>
             </div>
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,40 @@
+import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import qZone1 from '../../../assets/qZone1.png';
+import qZone2 from '../../../assets/qZone2.png';
+import qZone3 from '../../../assets/qZone3.png';
 
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right SideNav</h2>
+            <div>
+                <h2 className="text-xl font-semibold">Login With</h2>
+                <button className="btn btn-outline w-full mt-3"><FaGoogle />  Login With Google</button>
+                <button className="btn btn-outline w-full mt-3"><FaGithub />  Login With Github</button>
+            </div>
+
+            <div className="mt-7">
+                <h2 className="text-[#403F3F] font-semibold text-xl">Find Us On</h2>
+                <a href="" className="flex items-center gap-2 mt-7 px-4 py-3 border rounded-lg">
+                    <FaFacebookF />
+                    Facebook
+                </a>
+                <a href="" className="flex items-center gap-2 px-4 py-3 border rounded-lg">
+                    <FaTwitter />
+                    Twitter
+                </a>
+                <a href="" className="flex items-center gap-2 px-4 py-3 border rounded-lg">
+                    <FaInstagram />
+                    Instagram
+                </a>
+            </div>
+
+            <div className="bg-[#F3F3F3] mt-7 border rounded-md">
+                <h2 className="text-xl font-semibold my-4 ml-2">Q-Zone</h2>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+            </div>
         </div>
     );
 };
